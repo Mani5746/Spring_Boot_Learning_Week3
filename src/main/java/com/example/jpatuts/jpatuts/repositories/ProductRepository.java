8e273447b5fa418fa05330d349f6046faa4d5b03
Mani5746 <srivastavmanish389@gmail.com>
2025-12-29 23:34:09 +0530

    Creating some functions with help of SPring JPA and writing our own Queries

--- src/main/java/com/example/jpatuts/jpatuts/repositories/ProductRepository.java
+++ src/main/java/com/example/jpatuts/jpatuts/repositories/ProductRepository.java
@@ -2,6 +2,24 @@ package com.example.jpatuts.jpatuts.repositories;
 
 import com.example.jpatuts.jpatuts.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<ProductEntity,Long>{
+
+import com.example.jpatuts.jpatuts.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByTitle(String title);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+
+    @Query("select e from ProductEntity e where e.title=?1 and e.price=?2")
+    Optional<ProductEntity>findByTitleAndPrice(String title, BigDecimal price);
 }
--- src/test/java/com/example/jpatuts/jpatuts/JpatutsApplicationTests.java
+++ src/test/java/com/example/jpatuts/jpatuts/JpatutsApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.jpatuts.jpatuts;
 
+import com.example.jpatuts.jpatuts.entities.ProductEntity;
+import com.example.jpatuts.jpatuts.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class JpatutsApplicationTests {
+@Autowired
+	ProductRepository productRepository;
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository(){
+	ProductEntity productEntity=ProductEntity.builder()
+			.sku("nestle234")
+			.title("Nestle Chocolate")
+			.price(BigDecimal.valueOf(123.345))
+			.quantity(13)
+			.build();
+
+	ProductEntity savedProductEntity=productRepository.save(productEntity);
+		System.out.println(savedProductEntity);
+
+	}
+
+	@Test
+	void getRepository(){
+		List<ProductEntity> entities=productRepository.findByCreatedAtAfter(
+				LocalDateTime.of(2024,1,1,0,0,0));
+		System.out.println(entities);
+	}
+@Test
+	void getRepositoryByTitle(){
+		List<ProductEntity> entities=productRepository.findByTitle("ParleGBiscuit");
+		System.out.println(entities);
+	}
+@Test
+	void findByTitleAndPrice(){
+
+		Optional<ProductEntity> entities=productRepository.findByTitleAndPrice("MirindaDrink", BigDecimal.valueOf(14.7));
 	}
 
 }
